11b5d89af0ecee24a5db5f6960100084cc257c1f
Stanley.Luo <luoyi@kobox.tv>
2016-01-13 10:07:10 +0800

    reformat

--- libs/KOPluginOpenSDK/src/cn/vszone/ko/plugin/sdk/KoLoadPluginActivity.java
+++ libs/KOPluginOpenSDK/src/cn/vszone/ko/plugin/sdk/KoLoadPluginActivity.java
@@ -20,7 +20,6 @@ import android.util.Log;
 import cn.vszone.ko.plugin.framework.PluginLoadListener;
 import cn.vszone.ko.plugin.framework.utils.PluginOpener;
 import cn.vszone.ko.plugin.sdk.KoStartUpActivity.ExitStartUpBroadcastReceiver;
-import cn.vszone.ko.plugin.sdk.misc.BranchConfig;
 import cn.vszone.ko.plugin.sdk.misc.PartnerAppIDs;
 import cn.vszone.ko.plugin.sdk.util.AppUtils;
 
@@ -37,19 +36,18 @@ public class KoLoadPluginActivity extends Activity {
     // ===========================================================
     // Constants
     // ===========================================================
-    public static final String KEY_FILE_PATH  = "key_file_path";
+    public static final String  KEY_FILE_PATH  = "key_file_path";
 
-    public static final String KEY_FILE_NAME  = "key_file_name";
-    public static final String KEY_START_TIME = "startTime";
-    private static final String DCKEY="85409328C0493F4219A18E27A544383D";
-    private static final String CHANNEL="Plugin_189store";
+    public static final String  KEY_FILE_NAME  = "key_file_name";
+    public static final String  KEY_START_TIME = "startTime";
+    private static final String DCKEY          = "85409328C0493F4219A18E27A544383D";
     // ===========================================================
     // Fields
     // ===========================================================
-    private String             mPluginDir;
-    private String             mApkName;
-    private long 			   mStartTime;
-    private long               mStartPluginTime;
+    private String              mPluginDir;
+    private String              mApkName;
+    private long                mStartTime;
+    private long                mStartPluginTime;
 
     // ===========================================================
     // Constructors
@@ -70,14 +68,14 @@ public class KoLoadPluginActivity extends Activity {
         Bundle bundle = getIntent().getExtras();
         mPluginDir = bundle.getString(KEY_FILE_PATH);
         mApkName = bundle.getString(KEY_FILE_NAME);
-        mStartTime=bundle.getLong(KEY_START_TIME);
-        //统计 启动插件加载页面的时间
-        Map<String,String> map=new HashMap<String,String>();
-        long endTime=System.currentTimeMillis();
-        long duration=endTime-mStartTime;
-        map.put("startTime", ""+mStartTime);
-        map.put("endTime", ""+endTime);
-        map.put("duration", ""+duration);
+        mStartTime = bundle.getLong(KEY_START_TIME);
+        // 统计 启动插件加载页面的时间
+        Map<String, String> map = new HashMap<String, String>();
+        long endTime = System.currentTimeMillis();
+        long duration = endTime - mStartTime;
+        map.put("startTime", "" + mStartTime);
+        map.put("endTime", "" + endTime);
+        map.put("duration", "" + duration);
         DCEvent.onEvent("startLoadPluginAct", map);
         loadPlugin();
     }
@@ -97,24 +95,24 @@ public class KoLoadPluginActivity extends Activity {
         }
         File file = new File(mPluginDir + "/" + mApkName);
         if (file.exists()) {
-        	mStartPluginTime=System.currentTimeMillis();
+            mStartPluginTime = System.currentTimeMillis();
             PluginOpener.startPlugin(this, mPluginDir + "/" + mApkName, useHostNativeLibs, new MyPluginLoadListener());
         } else {
             exit(1);
         }
     }
-    
+
     @Override
     protected void onResume() {
-    	super.onResume();
-    	Log.d("DCLOG", "onResume"+getClass().getSimpleName());
-    	DCAgent.onResume(this);
+        super.onResume();
+        Log.d("DCLOG", "onResume" + getClass().getSimpleName());
+        DCAgent.onResume(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("DCLOG", "onPause"+getClass().getSimpleName());
+        Log.d("DCLOG", "onPause" + getClass().getSimpleName());
         DCAgent.onPause(this);
         Log.d("TAG", "onPause");
     }
@@ -127,29 +125,29 @@ public class KoLoadPluginActivity extends Activity {
 
     private void exit(int pResult) {
         Log.d("TAG", "exit:" + pResult);
-      	//统计 启动插件的启动时间 成功和失败次数
-        Map<String,String> map=new HashMap<String,String>();
-        long endTime=System.currentTimeMillis();
-        map.put("startTime", ""+mStartPluginTime);
-        map.put("endTime", ""+endTime);
-        map.put("duration", ""+(endTime-mStartTime));
-        if(pResult==KoLoadPluginActivity.RESULT_OK){
-        	map.put("result", "success");
-        }else{
-        	map.put("result", "fail");
-        	map.put("erroMsg", "plugin file is no exists");
+        // 统计 启动插件的启动时间 成功和失败次数
+        Map<String, String> map = new HashMap<String, String>();
+        long endTime = System.currentTimeMillis();
+        map.put("startTime", "" + mStartPluginTime);
+        map.put("endTime", "" + endTime);
+        map.put("duration", "" + (endTime - mStartTime));
+        if (pResult == KoLoadPluginActivity.RESULT_OK) {
+            map.put("result", "success");
+        } else {
+            map.put("result", "fail");
+            map.put("erroMsg", "plugin file is no exists");
         }
         DCEvent.onEvent("startPlugin", map);
         finish();
     }
-    
+
     @Override
     protected void onDestroy() {
-    	super.onDestroy();
-    	//发出广播 让竞技台加载界面退出
-    	Intent intent=new Intent();
-    	intent.setAction(ExitStartUpBroadcastReceiver.FILTER_ACTION);
-    	sendBroadcast(intent);
+        super.onDestroy();
+        // 发出广播 让竞技台加载界面退出
+        Intent intent = new Intent();
+        intent.setAction(ExitStartUpBroadcastReceiver.FILTER_ACTION);
+        sendBroadcast(intent);
     }
 
     // ===========================================================
